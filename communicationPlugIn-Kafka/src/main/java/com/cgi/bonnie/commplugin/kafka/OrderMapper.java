be17fc714c7560df40aed6acf06f9f2bb48a8e2f
Gergely Topolyai <gergely.topolyai@cgi.com>
2023-01-24 08:07:42 +0100

    Add Json Schema for OrderJson, generate the code instead of the manually created

--- communicationPlugIn-Kafka/src/main/java/com/cgi/bonnie/commplugin/kafka/OrderMapper.java
+++ communicationPlugIn-Kafka/src/main/java/com/cgi/bonnie/commplugin/kafka/OrderMapper.java
@@ -1,20 +1,28 @@
 package com.cgi.bonnie.commplugin.kafka;
 
 import com.cgi.bonnie.businessrules.order.Order;
+import com.cgi.bonnie.schema.OrderJson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class OrderMapper {
+    private final Logger log = LoggerFactory.getLogger(OrderMapper.class);
+
     public static Order fromOrderJson(OrderJson orderJson) {
         return new Order().withShopOrderId(orderJson.getShopOrderId())
                 .withGoodsId(orderJson.getGoodsId())
                 .withQuantity(orderJson.getQuantity())
                 .withPlacementDate(orderJson.getPlacementDate())
-                .withMetadata(orderJson.getMetadataAsText());
+                .withMetadata(orderJson.getMetadata());
     }
 
-    public static List<Order> fromOrderJsonList(List<OrderJson> orderJsons) {
-        return orderJsons.stream().map(OrderMapper::fromOrderJson).collect(Collectors.toList());
+    public List<Order> fromOrderJsonList(List<OrderJson> orderJsons) {
+        List<Order> orders = orderJsons.stream().filter(Objects::nonNull).map(OrderMapper::fromOrderJson).collect(Collectors.toList());
+        log.info("There are {} income order and parsed {} items", orderJsons.size(), orders.size());
+        return orders;
     }
 }
