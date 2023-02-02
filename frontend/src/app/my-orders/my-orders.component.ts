5cd97a8254486f144334d1528e57810b7270c432
Nikita <mrk.moroka@gmail.com>
2023-02-02 16:10:17 +0100

    Remove previous filter from 'my-orders' component. Make it show only not shipped orders

--- frontend/src/app/my-orders/my-orders.component.ts
+++ frontend/src/app/my-orders/my-orders.component.ts
@@ -15,11 +15,9 @@ export class MyOrdersComponent implements OnInit {
   ngOnInit(): void {
     this.orderControllerService.getMyOrders().subscribe(myOrders => {
       this.orders = myOrders.filter(myOrder => {
-        let now = new Date().getTime();
-        let oneDaySinceLastUpdate = new Date(myOrder.lastUpdate!.toString()).getTime() + 86400000;
-        return (oneDaySinceLastUpdate > now && myOrder.status == 'SHIPPED') || (myOrder.status != 'SHIPPED');
+        return (myOrder.status != 'SHIPPED');
       });
-    });    
-  }  
+    });
+  }
 
 }
